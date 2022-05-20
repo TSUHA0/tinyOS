c6ec8c66ef99831ae04e0191c3f738a83b138092
tsuhao <1355738338@qq.com>
2022-05-20 14:18:23 +0800

    init

---
+++ src/read_args.cpp
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    assert(argv[i]); // C 标准保证
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  assert(!argv[argc]); // C 标准保证
+  return 0;
+}
