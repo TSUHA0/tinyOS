87efac57a6df1f1df4bd0e07037a540d9abbc84b
root <root@TsuhaoLaptop-XX.localdomain>
2022-05-22 18:51:57 +0800

    add fish.c

--- src/pc_cond.c
+++ src/pc_cond.c
@@ -5,26 +5,36 @@ int n, count = 0;
 mutex_t lk = MUTEX_INIT();
 cond_t cv = COND_INIT();
 
-void Tproduce() {
-  mutex_lock(&lk);
-  if (count == n) cond_wait(&cv, &lk);
-  printf("("); count++; cond_signal(&cv);
-  mutex_unlock(&lk);
+void Tproduce()
+{
+	mutex_lock(&lk);
+	if (count == n)
+		cond_wait(&cv, &lk);
+	printf("(");
+	count++;
+	cond_signal(&cv);
+	mutex_unlock(&lk);
 }
 
-void Tconsume() {
-  mutex_lock(&lk);
-  if (count == 0) cond_wait(&cv, &lk);
-  printf(")"); count--; cond_signal(&cv);
-  mutex_unlock(&lk);
+void Tconsume()
+{
+	mutex_lock(&lk);
+	if (count == 0)
+		cond_wait(&cv, &lk);
+	printf(")");
+	count--;
+	cond_signal(&cv);
+	mutex_unlock(&lk);
 }
 
-int main(int argc, char *argv[]) {
-  assert(argc == 2);
-  n = atoi(argv[1]);
-  setbuf(stdout, NULL);
-  for (int i = 0; i < 8; i++) {
-    create(Tproduce);
-    create(Tconsume);
-  }
+int main(int argc, char *argv[])
+{
+	assert(argc == 2);
+	n = atoi(argv[1]);
+	setbuf(stdout, NULL);
+	for (int i = 0; i < 8; i++)
+	{
+		create(Tproduce);
+		create(Tconsume);
+	}
 }
